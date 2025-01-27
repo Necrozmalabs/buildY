770a4f50679f67f4f9f8696e3fb76b9e393a1c61
Abhinav Sharma <85983956+legendaryabhi@users.noreply.github.com>
2025-01-27 11:15:33 +0530

    add ui

---
+++ ui/CardGrid.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import '../styles/CardGrid.css'; // Link to a CSS file for styling
+
+const Card = ({ gif, title, link }) => {
+  return (
+    <div className="card">
+      <a href={link} target="_blank" rel="noopener noreferrer" className="card-link">
+        <div className="card-content">
+          <div className="image-container" style={{ backgroundColor: 'white' }}>
+            <img src={gif} alt="Card Thumbnail" className="card-image" />
+          </div>
+          <div className="card-info">
+            <h3 className="card-title">{title}</h3>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+const CardGrid = ({ tag }) => {
+  console.log('Received tag:', tag);
+
+  
+  
+
+  // Filter cards based on the tag
+  const filteredCards = tag === 'all' ? cards : cards.filter((card) => card.tags.includes(tag));
+
+  return (
+    <div className="card-grid">
+      {filteredCards.length > 0 ? (
+        filteredCards.map((card, index) => <Card key={index} {...card} />)
+      ) : (
+        <p style={{ padding: '32px' }}>Nothing here</p>
+      )}
+    </div>
+  );
+};
+
+export default CardGrid;
